91c286f7b67e402a85301f7c441526bf2578f567
Finn <finnterdal@gmail.com>
2019-12-13 11:37:48 -0600

    finish lecture

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express")
+const session = require("express-session")
 const morgan = require("morgan")
+const html = require('html-template-tag')
 
 const app = express()
+
+app.use(session({
+  secret: 'sd78fg8sd7gyd8uyfgduifghj',
+  resave: false,
+  saveUninitialized: true,
+}))
+
 app.use(morgan("dev"))
 
 app.get("/", (req, res, next) => {
-  res.send(`
+  // const sessionCount = 1
+  if (!req.session.count) req.session.count = 1
+  else req.session.count++
+  const sessionCount = req.session.count
+  console.log(req.session)
+  res.send(html`
+    <h1 style="text-align: center;">SessionCount: ${sessionCount}</h1>
     <img src="/cookiemonster.jpeg">
   `)
 })
